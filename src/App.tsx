b7abf2d04f402180f6b4e3eff0c1cab5071b7b67
minu youn <anomie7777@gmail.com>
2021-06-13 15:42:00 +0900

    TodoTemplate 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import './App.css';
+import TodoTemplate from './components/TodoTemplate';
 
 const App: FunctionComponent = () => {
-  return <div>Todo App을 만들자</div>
+  return (
+    <TodoTemplate>Todo App을 만들자!</TodoTemplate>
+  )
 }
 
 export default App;
---
+++ src/components/TodoTemplate.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from "react";
+import './TodoTemplate.scss'
+
+const TodoTemplate : FunctionComponent = ({children}) => {
+    return (
+        <div className="TodoTemplate">
+            <div className="app-title">일정 관리</div>
+            <div className="content">{children}</div>
+        </div>
+    )
+}
+export default TodoTemplate
