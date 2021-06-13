e4ba2b81f82e8ffda2aaddb8805fe988ea128b0e
minu youn <anomie7777@gmail.com>
2021-06-13 16:55:56 +0900

    TodoLit와 TodoListItem 컴포넌트 구성

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React, { FunctionComponent } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App: FunctionComponent = () => {
   return (
     <TodoTemplate>
-      <TodoInsert/>
+      <TodoInsert />
+      <TodoList />
     </TodoTemplate>
-  )
-}
+  );
+};
 
 export default App;
